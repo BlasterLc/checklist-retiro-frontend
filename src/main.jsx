4183332c943de05f52e027a1816d8728401f51fe
kapriutska21 <matiadpa@gmail.com>
2026-06-01 15:48:00 -0400

    primera pagina lista

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './styles/main.scss';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-);
+import './index.css'
+import './styles/main.scss'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+)
