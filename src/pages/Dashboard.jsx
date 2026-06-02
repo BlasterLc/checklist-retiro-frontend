7482a61dcc3dceb57103a54c05bd978ff0550f08
kapriutska21 <matiadpa@gmail.com>
2026-06-02 18:42:12 -0400

    uso de libreria recharts y dashboard completo y funcional

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,56 @@
+import { useRetiros } from "../context/RetiroContext";
+
 function Dashboard() {
+
+  const { retiros } = useRetiros();
+
+  const totalRetiros = retiros.length;
+
+  const aprobados = retiros.filter(
+    retiro => retiro.estado === "Aprobado"
+  ).length;
+
+  const rechazados = retiros.filter(
+    retiro => retiro.estado === "Rechazado"
+  ).length;
+
+  const tasaAprobacion =
+    totalRetiros > 0
+      ? ((aprobados / totalRetiros) * 100).toFixed(1)
+      : 0;
+
   return (
-    <h2>Dashboard</h2>
+
+    <div className="dashboard">
+
+      <h1>Dashboard de Métricas</h1>
+
+      <div className="cards-metricas">
+
+        <div className="card-metrica">
+          <h3>Total Retiros</h3>
+          <p>{totalRetiros}</p>
+        </div>
+
+        <div className="card-metrica">
+          <h3>Aprobados</h3>
+          <p>{aprobados}</p>
+        </div>
+
+        <div className="card-metrica">
+          <h3>Rechazados</h3>
+          <p>{rechazados}</p>
+        </div>
+
+        <div className="card-metrica">
+          <h3>Tasa Aprobación</h3>
+          <p>{tasaAprobacion}%</p>
+        </div>
+
+      </div>
+
+    </div>
+
   );
 }
 
